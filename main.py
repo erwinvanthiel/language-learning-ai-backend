d7d0fb299fe881dc79333841ebfeab5858a33464
Erwin van Thiel <vanthiel.erwin@gmail.com>
2026-08-25 19:47:18 +0200

    Allow requests from the frontend PWA (#3)
    Co-authored-by: Erwin van Thiel <23634300+erwinvanthiel@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="Language Learning AI API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://yellow-coast-0325af203.7.azurestaticapps.net",
+    ],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
